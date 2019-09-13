b995bd0ae4d2c68adbaf22245661de4672261b66
Nicolas Alvarez Llobera <nalvarezllobera@navent.com>
2019-09-13 17:07:30 -0300

    agrego jars del proyecto que dio godio

--- src/main/java/com/uade/reclamos/controllers/EdificioController.java
+++ src/main/java/com/uade/reclamos/controllers/EdificioController.java
@@ -1,21 +1,25 @@
 package com.uade.reclamos.controllers;
 
 import controlador.Controlador;
+import exceptions.EdificioException;
+import exceptions.UnidadException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import views.EdificioView;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/edificios")
 public class EdificioController {
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public HttpEntity<BSREPostingSearchResult> listarEdificios() {
-        String pagingLimit = SEARCH_NAME_MAP.equals(searchName) ? SEARCH_NAME_MAP_POSTINGS_LIMIT : siteService.hasMoreDevelopmentsAndPostingLimit() ? POSTING_LIMIT_RELA : POSTING_LIMIT_DEFAULT;
-        return new ResponseEntity<>(postingService.getListado(sortByName, searchName, searchParameters, pagingOffset, pagingLimit,null, null), HttpStatus.OK);
+    public HttpEntity<List<EdificioView>> listarEdificios() throws UnidadException, EdificioException {
+        return new ResponseEntity<>(Controlador.getInstancia().getEdificios(), HttpStatus.OK);
     }
 
 }
