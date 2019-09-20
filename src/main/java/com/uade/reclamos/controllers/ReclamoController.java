540299f6b873fc261e32e0b14e4f131d58278c87
Nicolas Alvarez Llobera <nalvarezllobera@navent.com>
2019-09-19 21:52:24 -0300

    agrego endpoints de listar y crear reclamos

--- src/main/java/com/uade/reclamos/controllers/ReclamoController.java
+++ src/main/java/com/uade/reclamos/controllers/ReclamoController.java
@@ -1,30 +1,33 @@
 package com.uade.reclamos.controllers;
 
-import com.uade.reclamos.exceptions.ReclamoException;
-import com.uade.reclamos.model.Reclamo;
-import com.uade.reclamos.services.ReclamoService;
+import controlador.Controlador;
+import exceptions.ReclamoException;
+import modelo.Reclamo;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import views.ReclamoView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/reclamos")
 public class ReclamoController {
 
-    private final ReclamoService reclamoService;
-
-    public ReclamoController(ReclamoService reclamoService) {
-        this.reclamoService = reclamoService;
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public HttpEntity<List<ReclamoView>> listarTodosLosReclamos() throws ReclamoException {
+        return new ResponseEntity<>(Controlador.getInstancia().todosLosReclamos(), HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public HttpEntity<List<Reclamo>> listarTodosLosReclamos() throws ReclamoException {
-        return new ResponseEntity<>(reclamoService.listarTodosLosReclamos(), HttpStatus.OK);
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    public HttpEntity<Void> listarTodosLosReclamos(@RequestBody @Valid Reclamo reclamo) {
+        Controlador.getInstancia().generarReclamo(reclamo);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
--- src/main/java/com/uade/reclamos/exceptions/ReclamoException.java
+++
@@ -1,9 +0,0 @@
-package com.uade.reclamos.exceptions;
-
-public class ReclamoException extends Exception {
-
-    public ReclamoException(String mensaje) {
-        super(mensaje);
-    }
-
-}
