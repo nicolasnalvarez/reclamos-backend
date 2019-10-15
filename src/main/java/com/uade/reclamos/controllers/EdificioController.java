f6930982ea7c2df6adc2bb604ebc7b8a3632e42e
Nicolas Alvarez Llobera <nalvarezllobera@navent.com>
2019-10-15 10:39:35 -0300

    agrego endpoint de spring para listar edificios por dni y tipo de usuario

--- src/main/java/com/uade/reclamos/controllers/EdificioController.java
+++ src/main/java/com/uade/reclamos/controllers/EdificioController.java
@@ -2,14 +2,12 @@ package com.uade.reclamos.controllers;
 
 import controlador.Controlador;
 import exceptions.EdificioException;
+import exceptions.PersonaException;
 import exceptions.UnidadException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import views.EdificioView;
 import views.UnidadView;
 
@@ -22,6 +20,16 @@ public class EdificioController {
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public HttpEntity<List<EdificioView>> listarEdificios() throws UnidadException, EdificioException {
         return new ResponseEntity<>(Controlador.getInstancia().getEdificios(), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/personas", produces = "application/json")
+    public HttpEntity<List<EdificioView>> getEdificiosPorDocumentoYTipoUsuario(@RequestParam("documento") String documento,
+                                                                               @RequestParam("tipo_usuario") int tipoUsuario) throws PersonaException {
+        if (tipoUsuario == 1) {
+            return new ResponseEntity<>(Controlador.getInstancia().getEdificiosByDocumentoDuenio(documento), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(Controlador.getInstancia().getEdificiosByDocumentoInquilino(documento), HttpStatus.OK);
+        }
     }
 
 }
