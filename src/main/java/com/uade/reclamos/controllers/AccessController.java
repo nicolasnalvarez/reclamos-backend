bb521ca1f627e87ce9b37ed2886c926243f0658e
lgriotti <lgriotti@github.com>
2019-10-16 20:37:27 -0300

    listo endpoints, falta verificar el jar y el infer de la entity

--- src/main/java/com/uade/reclamos/controllers/AccessController.java
+++ src/main/java/com/uade/reclamos/controllers/AccessController.java
@@ -1,21 +1,29 @@
 package com.uade.reclamos.controllers;
-
+import com.uade.reclamos.model.LoginRequest;
+import com.uade.reclamos.model.RegisterRequest;
 import controlador.Controlador;
+
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import views.*;
 
 @RestController
+@RequestMapping
 public class AccessController {
 
 
-    @PostMapping(path = "/auth/login", produces = "application/json")
-    @ResponseStatus(HttpStatus.OK)
-    public @ResponseBody String loguear(@RequestBody UsuarioView usuario)  {
-        Controlador.getInstancia().login(usuario.getNombre(),usuario.getPaswword());
-        return "Usuario logueado";
+    @RequestMapping(method = RequestMethod.POST,path = "/auth/login", produces = "application/json")
+    public HttpEntity<UsuarioView> loguear(@RequestBody @Valid LoginRequest loginRequest) {
+        return new ResponseEntity<>(Controlador.getInstancia().login(loginRequest.getNombre(), loginRequest.getPassword()), HttpStatus.OK);
     }
 
+    @RequestMapping(method = RequestMethod.POST,path = "/auth/register", produces = "application/json")
+    public HttpEntity<Void> registrar(@RequestBody @Valid RegisterRequest registerRequest) {
+        Controlador.getInstancia().registrar(registerRequest.getDni(),registerRequest.getNombre(), registerRequest.getPassword());
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 
 
 }
