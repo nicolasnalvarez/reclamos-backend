9702d03371df78350dcce707deea10f76cfca720
Nicolas Alvarez Llobera <nalvarezllobera@navent.com>
2019-10-17 13:50:42 -0300

    cambios

--- src/main/java/com/uade/reclamos/controllers/AccessController.java
+++ src/main/java/com/uade/reclamos/controllers/AccessController.java
@@ -1,33 +1,42 @@
 package com.uade.reclamos.controllers;
+
 import com.uade.reclamos.model.LoginRequest;
 import com.uade.reclamos.model.RegisterRequest;
 import controlador.Controlador;
-
-
+import exceptions.CambioPasswordException;
+import exceptions.LoginException;
+import exceptions.PersonaException;
+import exceptions.UsuarioException;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import views.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import views.UsuarioView;
+
+import javax.validation.Valid;
 
 @RestController
-@RequestMapping
+@RequestMapping("/auth")
 public class AccessController {
 
-
-    @RequestMapping(method = RequestMethod.POST,path = "/auth/login", produces = "application/json")
-    public HttpEntity<UsuarioView> loguear(@RequestBody @Valid LoginRequest loginRequest) {
-        return new ResponseEntity<>(Controlador.getInstancia().login(loginRequest.getNombre(), loginRequest.getPassword()), HttpStatus.OK);
-    }
-
-    @RequestMapping(method = RequestMethod.POST,path = "/auth/register", produces = "application/json")
-    public HttpEntity<Void> registrar(@RequestBody @Valid RegisterRequest registerRequest) {
+    @RequestMapping(method = RequestMethod.POST,path = "/register", produces = "application/json")
+    public HttpEntity<Void> registrar(@RequestBody @Valid RegisterRequest registerRequest) throws PersonaException {
         Controlador.getInstancia().registrar(registerRequest.getDni(),registerRequest.getNombre(), registerRequest.getPassword());
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @RequestMapping(method = RequestMethod.POST,path = "/auth/changepass", produces = "application/json")
-    public HttpEntity<Void> cambioPassword(@RequestBody @Valid LoginRequest loginRequest) {
-        return new ResponseEntity<>(Controlador.getInstancia().cambioPassword(loginRequest.getNombre(), loginRequest.getPassword()), HttpStatus.OK);
+    @RequestMapping(method = RequestMethod.POST,path = "/login", produces = "application/json")
+    public HttpEntity<UsuarioView> login(@RequestBody @Valid LoginRequest loginRequest) throws LoginException, UsuarioException, CambioPasswordException {
+        return new ResponseEntity<>(Controlador.getInstancia().login(loginRequest.getNombre(), loginRequest.getPassword()), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,path = "/changepass", produces = "application/json")
+    public HttpEntity<Void> cambioPassword(@RequestBody @Valid LoginRequest loginRequest) throws CambioPasswordException, UsuarioException {
+        Controlador.getInstancia().cambioPassword(loginRequest.getNombre(), loginRequest.getPassword());
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
