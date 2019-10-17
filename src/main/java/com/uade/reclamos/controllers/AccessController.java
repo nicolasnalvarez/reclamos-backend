1dc8e4e695c795837ce678acf9578df9bb654b27
Nicolas Alvarez Llobera <nalvarezllobera@navent.com>
2019-10-17 17:55:16 -0300

    agrego cambios

--- src/main/java/com/uade/reclamos/controllers/AccessController.java
+++ src/main/java/com/uade/reclamos/controllers/AccessController.java
@@ -7,29 +7,31 @@ import exceptions.CambioPasswordException;
 import exceptions.LoginException;
 import exceptions.PersonaException;
 import exceptions.UsuarioException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import views.UsuarioView;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
+@Slf4j
 public class AccessController {
 
     @RequestMapping(method = RequestMethod.POST,path = "/register", produces = "application/json")
     public HttpEntity<Void> registrar(@RequestBody @Valid RegisterRequest registerRequest) throws PersonaException {
+        System.out.println("Se intenta registrar al usuario " + registerRequest.getNombre() + " con clave " + registerRequest.getPassword());
         Controlador.getInstancia().registrar(registerRequest.getDni(),registerRequest.getNombre(), registerRequest.getPassword());
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.POST,path = "/login", produces = "application/json")
     public HttpEntity<UsuarioView> login(@RequestBody @Valid LoginRequest loginRequest) throws LoginException, UsuarioException, CambioPasswordException {
+        System.out.println("Se intenta loguear al usuario " + loginRequest.getNombre() + " con clave " + loginRequest.getPassword());
         return new ResponseEntity<>(Controlador.getInstancia().login(loginRequest.getNombre(), loginRequest.getPassword()), HttpStatus.OK);
     }
 
